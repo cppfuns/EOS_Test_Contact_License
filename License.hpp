6c4fe645f5c4e77a1069c5bd2d17efa6f42dd5a1
suweigang <suweigang@kingsoft.com>
2018-06-07 18:00:23 +0800

    fix func changeUserInfo createProject changeProject

--- License.hpp
+++ License.hpp
@@ -6,25 +6,40 @@ class License : public eosio::contract {
     public:
         License( account_name self )
         :eosio::contract(self)
-        ,projects(_self, _self){}
+        ,projects(_self, _self)
+        ,accounts(_self, _self){}
 
-        void createProject(account_name owner, const std::string& project_name, const std::string& company_name, const std::string& contact_info);
+        void changeUserInfo(account_name owner, const std::string& company_name, const std::string& contact_info);
+        void createProject(account_name owner, const std::string& project_name);
         void changeProject(account_name owner, const std::string& project_name, const std::string& company_name, const std::string& contact_info);
         void buyLicense(account_name owner, const std::string& project_name, uint8_t license_type);
         void getLicense(account_name owner, const std::string& project_name, uint8_t license_type);
 
     private:
+
+        struct account{
+            account_name name;
+            std::string company_name;
+            std::string contact_info;
+            uint8_t level;
+            uint64_t primary_key()const { return name; }
+            EOSLIB_SERIALIZE(account, (name)(company_name)(contact_info)(level))
+        };
+
+        typedef eosio::multi_index<N(accounts),account> account_table;
+        account_table accounts;
+
         //@abi table projects i64
         struct project{
-            account_name owner;
             std::string project_name;
+            account_name owner;
             std::string company_name;
             std::string contact_info;
             uint8_t      status;
             std::string license_i;
             std::string license_u;
 
-            uint64_t primary_key()const { return owner; }
+            uint64_t primary_key()const { return project_name; }
             EOSLIB_SERIALIZE(project, (owner)(project_name)(company_name)(contact_info)(status)(license_i)(license_u))
         };
 
