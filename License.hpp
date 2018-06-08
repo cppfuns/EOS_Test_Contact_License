960f2d5977ff32288a700ae36ae3d139c1897b12
suweigang <suweigang@kingsoft.com>
2018-06-08 10:42:14 +0800

    Revert "start test"
    This reverts commit da0c8ed84d7c0bb36080a7916c59b84bc1100cf6.

--- License.hpp
+++ License.hpp
@@ -6,6 +6,7 @@ class License : public eosio::contract {
     public:
         License( account_name self )
         :eosio::contract(self)
+        ,accounts(_self, _self)
         ,projects(_self, _self){}
 
         void setuserinfo(account_name owner, const std::string& company_name, const std::string& contact_info);
@@ -15,14 +16,31 @@ class License : public eosio::contract {
         void getlicense(account_name owner, const std::string& project_name, uint8_t license_type);
 
     private:
-        //@abi table projects i64
-        struct project{
-            account_name owner;
+
+        struct account{
+            account_name name;
             std::string company_name;
             std::string contact_info;
             uint8_t level;
+            uint64_t primary_key()const { return name; }
+            EOSLIB_SERIALIZE(account, (name)(company_name)(contact_info)(level))
+        };
+
+        typedef eosio::multi_index<N(accounts),account> account_table;
+        account_table accounts;
+
+        //@abi table projects i64
+        struct project{
+            account_name owner;
+            std::string project_name;
+            std::string company_name;
+            std::string contact_info;
+            uint8_t      status;
+            std::string license_i;
+            std::string license_u;
+
             uint64_t primary_key()const { return owner; }
-            EOSLIB_SERIALIZE(project, (owner)(company_name)(contact_info)(level))
+            EOSLIB_SERIALIZE(project, (owner)(project_name)(company_name)(contact_info)(status)(license_i)(license_u))
         };
 
         typedef eosio::multi_index<N(projects),project> project_table;
