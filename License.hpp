fed91ac148796ee7219686dfc423d23c45d48424
suweigang <suweigang@kingsoft.com>
2018-06-08 10:19:45 +0800

    1

--- License.hpp
+++ License.hpp
@@ -28,6 +28,21 @@ class License : public eosio::contract {
         typedef eosio::multi_index<N(accounts),account> account_table;
         account_table accounts;
 
-     
+        //@abi table projects i64
+        struct project{
+            account_name owner;
+            std::string project_name;
+            std::string company_name;
+            std::string contact_info;
+            uint8_t      status;
+            std::string license_i;
+            std::string license_u;
+
+            uint64_t primary_key()const { return owner; }
+            EOSLIB_SERIALIZE(project, (owner)(project_name)(company_name)(contact_info)(status)(license_i)(license_u))
+        };
+
+        typedef eosio::multi_index<N(projects),project> project_table;
+        project_table projects;
 };
 EOSIO_ABI(License, (setuserinfo)(makeproject)(setproject)(buylicense)(getlicense))
