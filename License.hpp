08132b7bb2a4ce983b358482cace3c51919ed8c2
suweigang <suweigang@kingsoft.com>
2018-06-08 10:26:43 +0800

    1

--- License.hpp
+++ License.hpp
@@ -6,7 +6,6 @@ class License : public eosio::contract {
     public:
         License( account_name self )
         :eosio::contract(self)
-        ,accounts(_self, _self)
         ,projects(_self, _self){}
 
         void setuserinfo(account_name owner, const std::string& company_name, const std::string& contact_info);
@@ -17,30 +16,13 @@ class License : public eosio::contract {
 
     private:
 
-        struct account{
+        struct project{
             account_name owner;
             std::string company_name;
             std::string contact_info;
             uint8_t level;
             uint64_t primary_key()const { return owner; }
-            EOSLIB_SERIALIZE(account, (owner)(company_name)(contact_info)(level))
-        };
-
-        typedef eosio::multi_index<N(accounts),account> account_table;
-        account_table accounts;
-
-        //@abi table projects i64
-        struct project{
-            account_name owner;
-            std::string project_name;
-            std::string company_name;
-            std::string contact_info;
-            uint8_t      status;
-            std::string license_i;
-            std::string license_u;
-
-            uint64_t primary_key()const { return owner; }
-            EOSLIB_SERIALIZE(project, (owner)(project_name)(company_name)(contact_info)(status)(license_i)(license_u))
+            EOSLIB_SERIALIZE(project, (owner)(company_name)(contact_info)(level))
         };
 
         typedef eosio::multi_index<N(projects),project> project_table;
