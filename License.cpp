da0c8ed84d7c0bb36080a7916c59b84bc1100cf6
suweigang <suweigang@kingsoft.com>
2018-06-08 10:18:22 +0800

    start test

--- License.cpp
+++ License.cpp
@@ -1,41 +1,11 @@
 #include <License.hpp>
 
 void License::setuserinfo(account_name owner, const std::string& company_name, const std::string& contact_info){
-    eosio::print("License::setuserinfo line 4");
-    require_auth(owner);
-    auto account_itr = accounts.find(owner);
-    if(account_itr == accounts.end()){
-        account_itr = accounts.emplace(_self, [&](auto& account){
-            account.name = owner;
-            account.company_name = company_name;
-            account.contact_info = contact_info;
-        });
-    }
-    else{
-        accounts.modify(account_itr, 0, [&](auto& account) {
-            account.company_name = company_name;
-            account.contact_info = contact_info;
-        });
-    }
+    
 }
 
 void License::makeproject(account_name owner, const std::string& project_name){
-    require_auth(owner);
-    for( const auto& project : projects ) {
-        if(project.project_name == project_name && project.owner == owner){
-            eosio_assert(false, "owner with project already exists" );
-            return;
-        }
-    }
-    auto project_itr = projects.emplace(_self, [&](auto& project){
-        project.owner = owner;
-        project.project_name = project_name;
-        auto account_itr = accounts.find(owner);
-        if(account_itr == accounts.end()){
-            project.company_name = account_itr->company_name;
-            project.contact_info = account_itr->contact_info;
-        }
-    });
+
 }
 
 void License::setproject(account_name owner, const std::string& project_name, const std::string& company_name, const std::string& contact_info){
--- License.hpp
+++ License.hpp
@@ -6,8 +6,7 @@ class License : public eosio::contract {
     public:
         License( account_name self )
         :eosio::contract(self)
-        ,accounts(_self, _self)
-        ,projects(_self, _self){}
+        ,accounts(_self, _self){}
 
         void setuserinfo(account_name owner, const std::string& company_name, const std::string& contact_info);
         void makeproject(account_name owner, const std::string& project_name);
@@ -29,21 +28,6 @@ class License : public eosio::contract {
         typedef eosio::multi_index<N(accounts),account> account_table;
         account_table accounts;
 
-        //@abi table projects i64
-        struct project{
-            account_name owner;
-            std::string project_name;
-            std::string company_name;
-            std::string contact_info;
-            uint8_t      status;
-            std::string license_i;
-            std::string license_u;
-
-            uint64_t primary_key()const { return owner; }
-            EOSLIB_SERIALIZE(project, (owner)(project_name)(company_name)(contact_info)(status)(license_i)(license_u))
-        };
-
-        typedef eosio::multi_index<N(projects),project> project_table;
-        project_table projects;
+     
 };
 EOSIO_ABI(License, (setuserinfo)(makeproject)(setproject)(buylicense)(getlicense))
