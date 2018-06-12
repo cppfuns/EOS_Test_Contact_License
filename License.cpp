03af07bbd9ca0d93326ac30dfe68b5d7fe64eb36
suweigang <suweigang@kingsoft.com>
2018-06-12 18:20:49 +0800

    修改成 单用户 单项目

--- License.cpp
+++ License.cpp
@@ -21,22 +21,29 @@ void License::setuserinfo(account_name owner, const std::string& company_name, c
 
 void License::makeproject(account_name owner, const std::string& project_name){
     require_auth(owner);
-    for( const auto& project : projecttable ) {
-        if(project.project_name == project_name && project.owner_name == owner){
-            eosio_assert(false, "owner with project already exists" );
-            return;
-        }
-    }
-    auto project_itr = projecttable.emplace(_self, [&](auto& project){
-        project.owner_name = owner;
-        project.project_name = project_name;
-        auto account_itr = accounts.find(owner);
-        if(account_itr != accounts.end()){
+    //二级索引查找
+    auto customer_index = projecttable.template get_index<N(byowner_name)>();
+    account_name customer_acct = owner; //如果是string 可以用 eosio::string_to_name 转换
+    auto cust_itr = customer_index.find(customer_acct);
+    if (cust_itr != customer_index.end() && cust_itr->owner_name == customer_acct) {
+        customer_index.modify(cust_itr, 0, [&](auto& project) {
             project.company_name = account_itr->company_name;
             project.contact_info = account_itr->contact_info;
-            project.status = 0; //初始化未授权，须另购买
-        }
-    });
+            project.status = account_itr->status; //初始化未授权，须另购买
+        });
+    } 
+    else{
+        auto project_itr = projecttable.emplace(_self, [&](auto& project){
+                project.owner_name = owner;
+                project.project_name = project_name;
+                auto account_itr = accounts.find(owner);
+                if(account_itr != accounts.end()){
+                    project.company_name = account_itr->company_name;
+                    project.contact_info = account_itr->contact_info;
+                    project.status = 0; //初始化未授权，须另购买
+                }
+            });
+    }
 }
 
 void License::buylicense(account_name owner, const std::string& project_name){
