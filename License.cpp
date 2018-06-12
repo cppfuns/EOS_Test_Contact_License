d35b8f21e4a5a5017d569feedee7273b90a3f509
suweigang <suweigang@kingsoft.com>
2018-06-12 15:08:49 +0800

    1

--- License.cpp
+++ License.cpp
@@ -21,14 +21,14 @@ void License::setuserinfo(account_name owner, const std::string& company_name, c
 
 void License::makeproject(account_name owner, const std::string& project_name){
     require_auth(owner);
-    for( const auto& project : projects ) {
-        if(project.project_name == project_name && project.user_id == owner){
+    for( const auto& project : project_table ) {
+        if(project.project_name == project_name && project.owner_name == owner){
             eosio_assert(false, "owner with project already exists" );
             return;
         }
     }
-    auto project_itr = projects.emplace(_self, [&](auto& project){
-        project.user_id = owner;
+    auto project_itr = project_table.emplace(_self, [&](auto& project){
+        project.owner_name = owner;
         project.project_name = project_name;
         auto account_itr = accounts.find(owner);
         if(account_itr == accounts.end()){
@@ -42,11 +42,17 @@ void License::makeproject(account_name owner, const std::string& project_name){
 void License::buylicense(account_name owner, const std::string& project_name){
     require_auth(owner);
     bool bFind = false;
-    for( const auto& project : projects ) {
-        if(project.project_name == project_name && project.user_id == owner){
-            if(project.status == 0){
+
+    auto customer_index = project_table.template get_index<N(getowner_name)>();
+    account_name customer_acct = eosio::chain::string_to_name(owner);
+    auto cust_itr = customer_index.find(customer_acct);
+    while (cust_itr != service_table.end() && cust_itr->owner_name == customer_acct) {
+        if(cust_itr->project_name == project_name){
+            if(cust_itr->status == 0){
                 //TODO 判断当前账户余额，购买转账等操作
-                project.status = 1;
+                project_table.modify(cust_itr, 0, [&](auto& project) {
+                    project.status = 1;
+                });
             }
             else{
                 eosio_assert(false, "The current project is authorized" );
@@ -54,15 +60,16 @@ void License::buylicense(account_name owner, const std::string& project_name){
             bFind = true;
             break;
         }
-    }
+        cust_itr++;
+    } 
     eosio_assert(bFind, "owner with project not exists" );
 }
 
 void License::testlicense(account_name owner, const std::string& project_name){
     require_auth(owner);
     bool bFind = false;
-    for( const auto& project : projects ) {
-        if(project.project_name == project_name && project.user_id == owner){
+    for( const auto& project : project_table ) {
+        if(project.project_name == project_name && project.owner_name == owner){
             eosio_assert((project.status == 1), "The current project is not authorized" );
             bFind = true;
             break;
--- License.hpp
+++ License.hpp
@@ -30,24 +30,24 @@ class License : public eosio::contract {
 
         //@abi table projects i64
         struct project{
-            uint32_t project_rank;
-            account_name user_id;
+            uint32_t pkey;
+            account_name owner_name;
             std::string project_name;
             std::string company_name;
             std::string contact_info;
             uint8_t      status;
 
-            uint64_t primary_key()const { return project_rank; }
-            uint64_t user_key() const{return user_id;}
-            EOSLIB_SERIALIZE(project, (project_rank)(user_id)(project_name)(company_name)(contact_info)(status))
+            uint64_t primary_key()const { return pkey; }
+            uint64_t getowner_name() const{return owner_name;}
+            EOSLIB_SERIALIZE(project, (pkey)(owner_name)(project_name)(company_name)(contact_info)(status))
         };
         //contracts/eosiolib/multi_index.hpp
         //multi_index用于封装智能合约对数据库的管理，增、删、改、查
         //eosio::multi_index<N(表名),数据类型> 
         //一个multi_index最多支持16个二级索引
-        //定义二级索引使用eosio::indexed_by模板。N(projectUserId)为索引的名称
-        //project表数据类型，uint64_t索引类型即索引函数返回的类型(只支持uint64_t,uint128_t,double,long double,key256类型)，&project::user_key索引函数。
-        typedef eosio::multi_index<N(projects),project, eosio::indexed_by<N(projectUserId), eosio::const_mem_fun<project, uint64_t, &project::user_key> >> project_table;
-        project_table projects;
+        //定义二级索引使用eosio::indexed_by模板。N(byowner_name)为索引的名称
+        //project表数据类型，uint64_t索引类型即索引函数返回的类型(只支持uint64_t,uint128_t,double,long double,key256类型)，&project::getowner_name索引函数。
+        typedef eosio::multi_index<N(project_table),project, eosio::indexed_by<N(byowner_name), eosio::const_mem_fun<project, account_name, &project::getowner_name> >> project_table_type;
+        project_table_type project_table;
 };
 EOSIO_ABI(License, (setuserinfo)(makeproject)(buylicense)(testlicense))
